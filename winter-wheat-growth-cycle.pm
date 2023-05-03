09ab4eea5766ff232803fd442e6030cf6a3be9e4
Roberto Cesetti <robertocesetti9@gmail.com>
2023-05-03 12:22:36 +0200

    [FEATURE - TILLERS] Review of tillers production's rule and refactor of the codebase.

--- winter-wheat-growth-cycle.pm
+++ winter-wheat-growth-cycle.pm
@@ -1,10 +1,36 @@
+/*-------------------- Winter Wheat Growth Cycle --------------------*/
+
+/* _-_-_-_-_-_-_-_-_-_-_ Params _-_-_-_-_-_-_-_-_-_-_ */
+
+
+/* The percentage of nutrients in the soil (a value between 0 and 1) */
+param soil_nutrients = 0.5;
+
+/* Average daily water (mm) received by the plantation during its growth period (best conditions: 4-6mm) */
+param daily_water = 5;
+
+/* Average daily temperature (°C) during the growth cycle period (best conditions: 10-20°C) */
+param temperature = 5;
+
+
+/* _-_-_-_-_-_-_-_-_-_-_ Consts _-_-_-_-_-_-_-_-_-_-_ */
+
+
+const root_energy = 4;
+
+const daily_water_rate = 1/(1+(((daily_water-5))^4));
+
+const temperature_rate = 1/(1+(((temperature-20)/5)^2));
+
+
+/* _-_-_-_-_-_-_-_-_-_-_ Species _-_-_-_-_-_-_-_-_-_-_ */
+
+
 /* Seeds */
 species S;
 
 /* Rotten seeds */
 species RS;
-
-const root_energy = 4;
 
 /* Roots */
 species R of [0, root_energy];
@@ -12,18 +38,9 @@ species R of [0, root_energy];
 /* Tillers */
 species T;
 
-/* The percentage of nutrients in the soil (a value between 0 and 1) */
-param soil_nutrients = 0.5;
 
-/* Average daily temperature (°C) during the growth cycle period */
-param temperature = 15;
+/* _-_-_-_-_-_-_-_-_-_-_ Rules _-_-_-_-_-_-_-_-_-_-_ */
 
-/* Average daily water (mm) received by the plantation during its growth period */
-param daily_water = 4;
-
-const daily_water_rate = 1/(1+(((daily_water-5)/0.5)^4));
-
-const temperature_rate = 1/(1+(((temperature-15)/5)^6));
 
 /* The Seed produces Roots depending on soil nutrients */
 rule seed_produces_root {
@@ -39,6 +56,10 @@ rule seed_rots {
 rule roots_produce_tiller for i in [1, root_energy] {
 	R[i] -[temperature_rate * daily_water_rate]-> T | R[i-1]
 }
+
+
+/* _-_-_-_-_-_-_-_-_-_-_ Measures _-_-_-_-_-_-_-_-_-_-_ */
+
 
 measure seeds = #S;
 measure rotten_seed = #RS;
